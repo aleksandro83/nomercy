76a76acf17cac07fe9cfa5b69c7d74fabdeecc8f
Aleksandro Ribeiro <aleksandro.ribeiro@fastalelo.onmicrosoft.com>
2018-08-29 19:14:24 -0300

    added auto-merge.sh

---
+++ auto-merge.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Automatically merge the last commit in master to automaticMergeTest
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+LAST_COMMIT=$(git rev-list -1 HEAD)
+
+echo Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to automaticMergeTest
+
+git checkout automaticMergeTest
+git merge master
+git checkout $CURRENT_BRANCH
