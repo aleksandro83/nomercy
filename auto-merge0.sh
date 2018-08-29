7f303770d9d6371335add72f7c7403e17e046b30
Aleksandro Ribeiro <aleksandro.ribeiro@fastalelo.onmicrosoft.com>
2018-08-29 20:20:27 -0300

    UP A-M.SH

--- auto-merge0.sh
+++ auto-merge0.sh
@@ -9,3 +9,17 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # Success
 echo "-------------------------------------------------------------------------"
 echo "Merge master into $CURRENT_BRANCH complete"
+
+
+
+# // #!/usr/bin/env bash
+
+# // # Checkout master branch and merge version branch into master
+# // git checkout nomercy0
+# // git merge master --no-ff --no-edit
+# // git push
+# // CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# // # Success
+# // echo "-------------------------------------------------------------------------"
+# // echo "Merge master into $CURRENT_BRANCH complete"
